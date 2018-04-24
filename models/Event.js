6e55af8a374c79109278b42acd08116c8ac4169d
David Daza <03dazal@gmail.com>
2018-04-24 16:18:16 -0500

    Add events model

---
+++ models/Event.js
@@ -0,0 +1,27 @@
+const keystone = require('keystone');
+const Types = keystone.Field.Types;
+
+/**
+ * Event Model
+ * ==========
+ */
+
+const Event = new keystone.List('Event', {
+	map: { name: 'title' },
+	autokey: { path: 'slug', from: 'title', unique: true },
+});
+
+Event.add({
+	title: { type: String, required: true },
+	state: { type: Types.Select, options: 'draft, published, archived', default: 'draft', index: true },
+  publishedDate: { type: Types.Date, index: true, dependsOn: { state: 'published' } },
+  featured: { type: Types.Boolean },
+  inscription: { type: String, dependsOn: { featured: true } },
+	image: { type: Types.CloudinaryImage, folder: 'igualdata/', dependsOn: { featured: true } },
+  content: { type: Types.Markdown, wysiwyg: true, height: 400 },
+  post: { type: String }
+});
+
+
+Event.defaultColumns = 'title, state|20%, publishedDate|20%';
+Event.register();
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,10 +1,23 @@
 const keystone = require('keystone');
+const Event = keystone.list('Event').model;
 
 exports = module.exports = function (req, res) {
 
 	const view = new keystone.View(req, res);
 	const { locals } = res;
 	locals.title = 'Mundial de la igualdad';
+	locals.data = {}
+
+	view.on('init', function (next) {
+		Event.find({ state: 'published' }, function (err, results) {
+			if (err) {
+				return next(err)
+			}
+			locals.data.events = results.sort((a, b) => new Date(b) -  new Date(a));
+			locals.data.featured = locals.data.events.find(event => event.featured)
+			next()
+		})
+	})
 
 	// Render the view
 	view.render('index');
