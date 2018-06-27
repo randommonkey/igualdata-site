4a5fe96fa022438e6753e7efa288fb0a0c3d247a
David Daza <03dazal@gmail.com>
2018-06-27 14:00:30 -0500

    Change app title

--- routes/views/matches.js
+++ routes/views/matches.js
@@ -4,7 +4,7 @@ exports = module.exports = function (req, res) {
 	const view = new keystone.View(req, res);
 	const { locals } = res;
 
-	locals.title = 'Notas de partido | Mundial de la igualdad';
+	locals.title = 'Figus de la igualdad | Mundial de la igualdad';
 
 	view.render('matches');
 };
